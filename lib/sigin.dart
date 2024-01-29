fb6e0364dc41f5f5e32c0f19986e00c6237001b4
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-01-29 10:29:13 +0530

    firebase sign in added

--- lib/sigin.dart
+++ lib/sigin.dart
@@ -1,7 +1,11 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:xtudify2/home.dart';
 import 'package:xtudify2/login.dart';
+//firebase auth
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class sigin extends StatefulWidget {
   const sigin({super.key});
@@ -27,6 +31,12 @@ class _siginState extends State<sigin> {
 
     });
   }//eye icon varan
+  bool isValidEmail(String email) {
+    //check if email is valid
+    return RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  }
+   TextEditingController emailController = TextEditingController();
+   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +69,7 @@ class _siginState extends State<sigin> {
                   Padding(
                     padding: const EdgeInsets.only(left: 30,right: 30,bottom: 15,top: 10),
                     child: TextField(
-                      
+                      controller: emailController,
                       decoration: InputDecoration(hintText: "Email address",hintStyle: TextStyle(color: Colors.white,fontSize: 20),
                     enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,width: 1)),
                      focusedBorder:  OutlineInputBorder(borderSide: BorderSide(color: Colors.white,width: 1))
@@ -71,6 +81,7 @@ class _siginState extends State<sigin> {
                     padding: const EdgeInsets.only(left: 30,right: 30,bottom: 15),
                     child: 
                     TextField(
+                      controller: passwordController,
                       decoration: InputDecoration(
                         suffixIcon: IconButton(onPressed: toggleicon, icon: eyeicon),//eye icon varan
                         hintText: "Password",hintStyle: TextStyle(color: Colors.white,fontSize: 20),
@@ -85,24 +96,77 @@ class _siginState extends State<sigin> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 30,right: 30,bottom: 15),
-                    child: Container(
-                      width: double.infinity,//screen full botton varan
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Sigin",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
+                    child: GestureDetector(
+                      onTap: () async {
+                        
+                      //check if email is valid
+                    if (isValidEmail(emailController.text)) {
+                      //check if password is empty
+                      if (passwordController.text.isNotEmpty) {
+                        //sign in user
+                        
+                       await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        ).then((user) {
+                          //if user is signed in successfully
+                          //go to home page
+                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                        }).catchError((e) {
+                          //if error, show error message
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                'Invalid email or password',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          );
+                        });
+                      } else {
+                        //show error message
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                              'Password cannot be empty',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        );
+                      }
+                    } else {
+                      //show error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            'Please enter a valid email',
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                        ),
-                    ),
-                    
+                      );
+                    }  
+
+
+                      },
+                      child: Container(
+                        width: double.infinity,//screen full botton varan
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sigin",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black
+                          ),
+                          ),
+                      ),
+                      
+                      ),
                     ),
                   ),
                   Text(
