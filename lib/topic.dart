c7d66303067f4f40cb33b858cf0ad860924e5466
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-02-07 14:46:39 +0530

    all page created

--- lib/topic.dart
+++ lib/topic.dart
@@ -1,17 +1,17 @@
 import 'package:auth/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:xtudify2/topic.dart';
 
-class ModulePage extends StatefulWidget {
+class topic extends StatefulWidget {
   String docid;
-  ModulePage({super.key, required this.docid});
+  String docid2;
+  topic({super.key, required this.docid, required this.docid2});
 
   @override
-  State<ModulePage> createState() => _ModulePageState();
+  State<topic> createState() => _topicState();
 }
 
-class _ModulePageState extends State<ModulePage> {
+class _topicState extends State<topic> {
   TextEditingController nameController = TextEditingController();
   final String email = FirebaseAuth.instance.currentUser!.email
       .toString(); //current userinte email id
@@ -19,7 +19,7 @@ class _ModulePageState extends State<ModulePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Module'),
+        title: const Text('topic'),
       ),
       body: Stack(
         children: [
@@ -69,11 +69,12 @@ class _ModulePageState extends State<ModulePage> {
                               //add name and module to collection email
                               FirebaseFirestore.instance
                                   .collection(email)
-                                  .doc(widget.docid)
+                                  .doc(widget.docid2)
                                   .collection('modules')
+                                  .doc(widget.docid)
+                                  .collection('topic')
                                   .add({
                                 'name': nameController.text,
-                                'topic': '0',
                               });
                               nameController.clear();
                               Navigator.pop(context);
@@ -116,8 +117,10 @@ class _ModulePageState extends State<ModulePage> {
           StreamBuilder<QuerySnapshot>(
             stream: FirebaseFirestore.instance
                 .collection(email)
-                .doc(widget.docid)
+                .doc(widget.docid2)
                 .collection('modules')
+                .doc(widget.docid)
+                .collection('topic')
                 .snapshots(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
@@ -142,81 +145,65 @@ class _ModulePageState extends State<ModulePage> {
                 children: snapshot.data!.docs.map((document) {
                   return Padding(
                     padding: const EdgeInsets.only(top: 15),
-                    child: GestureDetector(
-                      onTap: () {
-                        //navigate  to module page using meterail page route
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => topic(
-                              docid: document.id,
-                              docid2: widget.docid,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        width: double.infinity,
-                        height: 87,
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(width: 2, color: Colors.white),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                    child: Container(
+                      width: double.infinity,
+                      height: 87,
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 2, color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        padding: EdgeInsets.all(8.0),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    document['name'],
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  SizedBox(height: 7),
-                                  Text(
-                                    document['topic'],
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                              //delete button
-                              GestureDetector(
-                                onTap: () {
-                                  //delete document
-                                  FirebaseFirestore.instance
-                                      .collection(email)
-                                      .doc(widget.docid)
-                                      .collection('modules')
-                                      .doc(document.id)
-                                      .delete();
-                                },
-                                child: Container(
-                                  width: 30,
-                                  height: 30,
-                                  decoration: ShapeDecoration(
-                                    shape: RoundedRectangleBorder(
-                                      side: BorderSide(
-                                          width: 2, color: Colors.white),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  child: const Icon(
-                                    Icons.delete,
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  document['name'],
+                                  style: TextStyle(
                                     color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
+                                SizedBox(height: 7),
+                              ],
+                            ),
+                            //delete button
+                            GestureDetector(
+                              onTap: () {
+                                //delete document
+                                FirebaseFirestore.instance
+                                    .collection(email)
+                                    .doc(widget.docid2)
+                                    .collection('modules')
+                                    .doc(widget.docid)
+                                    .collection('topic')
+                                    .doc(document.id)
+                                    .delete();
+                              },
+                              child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: ShapeDecoration(
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(
+                                        width: 2, color: Colors.white),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
